b82b6800821f1e0fee4779b5c4229af95c86c6e4
iki1uc <entertrainer@web.de>
2026-08-16 18:30:37 +0200

    Create tzz.js

---
+++ tzz.js
@@ -0,0 +1,7 @@
+AXIOM_TRI = {
+  alpha: ach.syn,        // fix
+  beta: ach.quant,       // moduliert
+  gamma: circle.raw,     // stabil aber offen
+  continium: 0.728,
+  pulse: roar.pulse      // false → Dreieck erlaubt
+}
